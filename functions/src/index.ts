267397c629e2e30b53d8848426226a0ae7626218
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2021-03-03 15:17:12 -0500

    Tweak variable and PREINSTALL wording.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -122,10 +122,9 @@ const extractInput = (snapshot: admin.firestore.DocumentSnapshot): any => {
 
 const analyzeComment = async (string: string): Promise<IAttributeScores> => {
   const client = new Client(config.apiKey);
-  var attributes = config.attributes.split(",");
   return await client.getScores(string, {
     doNotStore: config.doNotStore === "true",
-    attributes: attributes,
+    attributes: config.attributes.split(","),
   });
 };
 
